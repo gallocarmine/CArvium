46e90faf9c6a09c7b15c6ee48a2fc22673138a84
Carmine Gallo <gallo.carmine02@gmail.com>
2025-07-19 12:34:30 +0200

    feat(showroom-filter): add dynamic filter logic for showroom search

---
+++ src/main/java/controller/CarFilterServlet.java
@@ -0,0 +1,97 @@
+package controller;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+import model.modelloAuto.ModelloAuto;
+import model.modelloAuto.ModelloAutoDAO;
+import model.ordine.Ordine;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@WebServlet(name = "CarFilterServlet", value = "/common/CarFilterServlet")
+public class CarFilterServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        request.setAttribute("selectedBrand", request.getParameter("brand"));
+        request.setAttribute("selectedCategory", request.getParameter("category"));
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/view/common/car/car-showroom.jsp");
+        dispatcher.forward(request, response);
+    }
+
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String brand = request.getParameter("brand");
+        String category = request.getParameter("category");
+        String y = request.getParameter("year");
+        String minP = request.getParameter("min-price");
+        String maxP = request.getParameter("max-price");
+
+        if (isValid(brand) && isValid(category)) {
+
+            int year = 0;
+            int minPrice = 0;
+            int maxPrice = 0;
+
+            if (isValid(y)) { year = Integer.parseInt(y); }
+
+            if (isValid(minP) && isValid(maxP)) {
+
+                minPrice = Integer.parseInt(minP);
+                maxPrice = Integer.parseInt(maxP);
+            }
+
+            List<ModelloAuto> models = new ModelloAutoDAO().doRetrieveByFilter(brand, category, year, minPrice, maxPrice);
+
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
+            PrintWriter out = response.getWriter();
+
+            ObjectMapper mapper = new ObjectMapper();
+
+            if (models != null) {
+
+                ObjectNode info = mapper.createObjectNode();
+
+                List<String> brands = models.stream().map(ModelloAuto::getIDMarcaAuto).distinct().collect(Collectors.toList());
+                List<String> categories = models.stream().map(ModelloAuto::getCategoria).distinct().collect(Collectors.toList());
+                List<Integer> years = models.stream().map(ModelloAuto::getAnno).distinct().collect(Collectors.toList());
+                double min = models.stream().map(ModelloAuto::getPrezzo).reduce(Double::min).get();
+                double max = models.stream().map(ModelloAuto::getPrezzo).reduce(Double::max).get();
+
+                info.set("carsInfo", mapper.valueToTree(models));
+                info.set("brand", mapper.valueToTree(brands));
+                info.set("categories", mapper.valueToTree(categories));
+                info.set("years", mapper.valueToTree(years));
+                info.set("minPrice", mapper.valueToTree(min));
+                info.set("maxPrice", mapper.valueToTree(max));
+
+                out.write(mapper.writeValueAsString(info));
+            }
+            else {
+
+                ObjectNode error = mapper.createObjectNode();
+                error.put("error", "models info not found");
+                out.write(mapper.writeValueAsString(error));
+            }
+
+            out.flush();
+            out.close();
+        }
+    }
+
+    private boolean isValid(String parameter) {
+
+        return parameter != null && !parameter.trim().isEmpty();
+    }
+}
---
+++ src/main/webapp/script/showroomFilters.js
@@ -0,0 +1,277 @@
+function fetchFilterCar(brandFilter, categoryFilter) {
+
+    if (brandFilter != null) {
+
+        document.getElementById("brand-filter").value = brandFilter;
+    }
+
+    if (categoryFilter != null) {
+
+        document.getElementById('category-filter').value = categoryFilter;
+    }
+
+    const form = document.getElementById('filter-form');
+
+    const formData = new FormData(form);
+    const urlParams = new URLSearchParams(formData);
+    const basePath = window.location.pathname.split('/')[1];
+
+    return fetch(`/${basePath}/common/CarFilterServlet`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+        },
+        body: urlParams.toString()
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+}
+
+
+function filterCar(brandFilter, categoryFilter) {
+
+    fetchFilterCar(brandFilter, categoryFilter)
+        .then(data => {
+
+            changeOptionFilter(data);
+
+            const showroom = document.querySelector('.showroom-container');
+            showroom.textContent = '';
+
+            data.carsInfo.forEach(carInfo => {
+
+                const car = document.createElement('div');
+
+                const brand = document.createElement('h3');
+                brand.textContent = carInfo.idmarcaAuto;
+                brand.id = 'brand-title'
+                car.appendChild(brand);
+
+                const model = document.createElement('h2');
+                model.textContent = carInfo.id;
+                model.id = 'model-id';
+                car.appendChild(model);
+
+                const contextPath = '/' + window.location.pathname.split('/')[1];
+                const img = document.createElement('img');
+                const sanitizedId = carInfo.id.toLowerCase().replace(/\s+/g, '-');
+                img.src = `${contextPath}/images/home-car/models-auto/${sanitizedId}.png`
+                img.id = 'img-model';
+                car.appendChild(img);
+
+                const price = document.createElement('p');
+                price.textContent = carInfo.prezzo + ' $';
+                price.id = 'price-model'
+                car.appendChild(price);
+
+                const category = document.createElement('p');
+                category.textContent = carInfo.categoria;
+                category.id = 'category-title';
+                car.appendChild(category);
+
+                const year = document.createElement('p');
+                year.textContent = carInfo.anno;
+                year.id = 'year-model';
+                car.appendChild(year);
+
+                const description = document.createElement('p');
+                description.textContent = carInfo.descrizione;
+                description.id = 'description-model';
+                car.appendChild(description);
+
+                showroom.appendChild(car);
+            })
+
+        })
+        .catch(error => {
+            console.error('There was a problem with the fetch operation:', error);
+        });
+}
+
+
+function changeOptionFilter(data) {
+
+    const brand = document.getElementById('brand-filter').value;
+    const category = document.getElementById('category-filter').value;
+
+    const currentBrand = brand;
+    const currentcategory = category;
+
+    if (brand === 'all' && category === 'all') {
+
+        const categoryFilter = document.getElementById('category-filter');
+        categoryFilter.textContent = '';
+
+        const optioncategory = document.createElement('option');
+        optioncategory.value = 'all';
+        optioncategory.textContent = 'All';
+        categoryFilter.appendChild(optioncategory);
+
+        data.categories.forEach(category => {
+            const opt = document.createElement('option');
+            opt.textContent = category[0].toUpperCase() + category.slice(1).toLowerCase();
+            opt.value = category;
+            categoryFilter.appendChild(opt);
+        });
+
+        const brandFilter = document.getElementById('brand-filter');
+        brandFilter.textContent = '';
+
+        const optionBrand = document.createElement('option');
+        optionBrand.value = 'all';
+        optionBrand.textContent = 'All';
+        brandFilter.appendChild(optionBrand);
+
+        data.brand.forEach(brand => {
+            const opt = document.createElement('option');
+            opt.textContent = brand[0].toUpperCase() + brand.slice(1).toLowerCase();
+            opt.value = brand;
+            brandFilter.appendChild(opt);
+        });
+    } else {
+
+        if (brand !== 'all' && category === 'all') {
+
+            const categoryFilter = document.getElementById('category-filter');
+            categoryFilter.textContent = '';
+
+            const option = document.createElement('option');
+            option.value = 'all';
+            option.textContent = 'All';
+            categoryFilter.appendChild(option);
+
+            data.categories.forEach(category => {
+                const opt = document.createElement('option');
+                opt.textContent = category[0].toUpperCase() + category.slice(1).toLowerCase();
+                opt.value = category;
+                categoryFilter.appendChild(opt);
+            });
+
+            if ([...categoryFilter.options].some(opt => opt.value === currentcategory)) {
+                categoryFilter.value = currentcategory;
+            }
+
+        } else {
+
+            fetchFilterCar(brand, 'all')
+                .then(newData => {
+                    insertDefaultOption('category-filter', newData.categories);
+
+                    const categoryFilter = document.getElementById('category-filter');
+                    if ([...categoryFilter.options].some(opt => opt.value === currentcategory)) {
+                        categoryFilter.value = currentcategory;
+                    }
+                });
+        }
+
+        if (category !== 'all' && brand === 'all') {
+
+            const brandFilter = document.getElementById('brand-filter');
+            brandFilter.textContent = '';
+
+            const option = document.createElement('option');
+            option.value = 'all';
+            option.textContent = 'All';
+            brandFilter.appendChild(option);
+
+            data.brand.forEach(brand => {
+                const opt = document.createElement('option');
+                opt.textContent = brand[0].toUpperCase() + brand.slice(1).toLowerCase();
+                opt.value = brand;
+                brandFilter.appendChild(opt);
+            });
+
+            if ([...brandFilter.options].some(opt => opt.value === currentBrand)) {
+                brandFilter.value = currentBrand;
+            }
+
+        } else {
+            fetchFilterCar('all', category)
+                .then(newData => {
+                    insertDefaultOption('brand-filter', newData.brand);
+
+                    const brandFilter = document.getElementById('brand-filter');
+                    if ([...brandFilter.options].some(opt => opt.value === currentBrand)) {
+                        brandFilter.value = currentBrand;
+                    }
+                });
+        }
+    }
+
+    const yearFilter = document.getElementById('year-filter');
+    const currentYear = yearFilter.value;
+    yearFilter.textContent = '';
+
+    if (currentYear === 0) {
+
+        const defaultYear = document.createElement('option');
+        defaultYear.textContent = 'All';
+        defaultYear.value = 0;
+        yearFilter.appendChild(defaultYear);
+
+        data.years.forEach(year => {
+
+            const option = document.createElement('option');
+            option.textContent = year;
+            option.value = year;
+
+            yearFilter.appendChild(option);
+        })
+
+        if ([...yearFilter.options].some(opt => opt.value === currentYear)) {
+            yearFilter.value = currentYear;
+        }
+    } else {
+
+        fetchFilterCar(brand, category)
+            .then(newData => {
+
+                const defaultYear = document.createElement('option');
+                defaultYear.textContent = 'All';
+                defaultYear.value = 0;
+                yearFilter.appendChild(defaultYear);
+
+                newData.years.forEach(year => {
+
+                    const option = document.createElement('option');
+                    option.textContent = year;
+                    option.value = year;
+
+                    yearFilter.appendChild(option);
+                })
+
+                if ([...yearFilter.options].some(opt => opt.value === currentYear)) {
+                    yearFilter.value = currentYear;
+                }
+            });
+    }
+
+    const minPrice = document.getElementById('min-price');
+    minPrice.placeholder = data.minPrice;
+
+    const maxPrice = document.getElementById('max-price');
+    maxPrice.placeholder = data.maxPrice;
+}
+
+
+function insertDefaultOption(id, options) {
+
+    const select = document.getElementById(id);
+    select.textContent = '';
+
+    const option = document.createElement('option');
+    option.value = 'all';
+    option.textContent = 'All';
+    select.appendChild(option);
+
+    options.forEach(opt => {
+        const option = document.createElement('option');
+        option.value = opt;
+        option.textContent = opt[0].toUpperCase() + opt.slice(1).toLowerCase();
+        select.appendChild(option);
+    });
+}
